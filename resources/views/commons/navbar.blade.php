402b222a2cc0e7625a12a90160862e16a2a17525
EC2 Default User <ec2-user@ip-172-31-40-229.ap-northeast-1.compute.internal>
2020-04-01 01:23:53 +0000

    ver1.01

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -1,6 +1,6 @@
 <header class="mb-4">
     <nav class="navbar navbar-expand-sm navbar-dark bg-dark"> 
-        <a class="navbar-brand" href="/">Connect ver1.00</a>
+        <a class="navbar-brand" href="/">Connect ver1.01</a>
          
         <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
             <span class="navbar-toggler-icon"></span>
